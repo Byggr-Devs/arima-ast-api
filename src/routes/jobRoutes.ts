39d9aa7d738fec50b1c87a691796b105c0ba286b
Robin J <robin.j@commerceiq.ai>
2023-09-23 16:30:39 +0530

    update job

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -54,23 +54,73 @@ router.post("/job", async (req, res) => {
 });
 
 router.get("/registrationParams", async (req, res) => {
-  // const serviceTypes = await prisma.serviceType.findMany();
-  const serviceCenters = await prisma.serviceCenter.findMany({include: {serviceTypes: true}});
+  const serviceCenters = await prisma.serviceCenter.findMany({
+    include: { serviceTypes: true },
+  });
   const priorities = ["HIGH", "MEDIUM", "LOW", "URGENT"];
 
-  res.send({ data: { serviceTypes: serviceCenters[0].serviceTypes, serviceCenters, priorities } });
+  res.send({
+    data: {
+      serviceTypes: serviceCenters[0].serviceTypes,
+      serviceCenters,
+      priorities,
+    },
+  });
 });
 
 router.get("/tracking", async (req, res) => {
-    const jobs = await prisma.jobRegistration.findMany({
-        include: {
-        serviceTypes: { include: { serviceType: true } },
-        },
-    });
-    
-    res.send({ data: jobs });
-    }
-);
+  const jobs = await prisma.jobRegistration.findMany({
+    include: {
+      serviceTypes: { include: { serviceType: true } },
+    },
+  });
+
+  res.send({ data: jobs });
+});
+
+router.post("/update-job", async (req, res) => {
+  const {
+    jobId,
+    waitingStageStatus,
+    stageOneStatus,
+    stageTwoStatus,
+    stageThreeStatus,
+    waterWashStageStatus,
+  } = req.body;
+
+  const job = await prisma.jobRegistration.update({
+    where: {
+      id: jobId,
+    },
+    data: {
+      waitingStageStatus,
+      stageOneStatus,
+      stageTwoStatus,
+      stageThreeStatus,
+      waterWashStageStatus,
+    },
+    include: {
+      serviceTypes: { include: { serviceType: true } },
+    },
+  });
+
+  res.send({ data: job });
+});
+
+router.get("/jobs-filtered", async (req, res) => {
+  const { stage } = req.query;
+
+  const jobs = await prisma.jobRegistration.findMany({
+    where: {
+      
+    },
+    include: {
+      serviceTypes: { include: { serviceType: true } },
+    },
+  });
+
+  res.send({ data: jobs });
+});
 
 router.get("/setup-db", async (req, res) => {
   const r = await prisma.serviceCenter.create({
@@ -84,11 +134,11 @@ router.get("/setup-db", async (req, res) => {
       zone: "South",
       serviceTypes: {
         create: [
-          {name: "Service Type 1"},
-          {name: "Service Type 2"},
-          {name: "Service Type 3"},
-        ]
-      }
+          { name: "Service Type 1" },
+          { name: "Service Type 2" },
+          { name: "Service Type 3" },
+        ],
+      },
     },
   });
   res.send({ data: r });
