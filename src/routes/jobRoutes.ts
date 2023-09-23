b35234dd511ee55f94d7db9648259692b5c52c6e
Robin J <robin.j@commerceiq.ai>
2023-09-23 15:55:28 +0530

    initial commit

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -54,11 +54,11 @@ router.post("/job", async (req, res) => {
 });
 
 router.get("/registrationParams", async (req, res) => {
-  const serviceTypes = await prisma.serviceType.findMany();
-  const serviceCenters = await prisma.serviceCenter.findMany();
+  // const serviceTypes = await prisma.serviceType.findMany();
+  const serviceCenters = await prisma.serviceCenter.findMany({include: {serviceTypes: true}});
   const priorities = ["HIGH", "MEDIUM", "LOW", "URGENT"];
 
-  res.send({ data: { serviceTypes, serviceCenters, priorities } });
+  res.send({ data: { serviceTypes: serviceCenters[0].serviceTypes, serviceCenters, priorities } });
 });
 
 router.get("/tracking", async (req, res) => {
@@ -71,5 +71,27 @@ router.get("/tracking", async (req, res) => {
     res.send({ data: jobs });
     }
 );
+
+router.get("/setup-db", async (req, res) => {
+  const r = await prisma.serviceCenter.create({
+    data: {
+      id: "service-center-1",
+      name: "Service Center 1",
+      city: "Chennai",
+      country: "India",
+      pincode: "600001",
+      state: "Tamil Nadu",
+      zone: "South",
+      serviceTypes: {
+        create: [
+          {name: "Service Type 1"},
+          {name: "Service Type 2"},
+          {name: "Service Type 3"},
+        ]
+      }
+    },
+  });
+  res.send({ data: r });
+});
 
 export default router;
